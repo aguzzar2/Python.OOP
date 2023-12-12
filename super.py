6e5c9a376c07f2b97ae1fbd2bcebd88fed6561c1
Anthony Guzzardo <aguzzar2@depaul.edu>
2023-12-12 11:10:06 -0600

    super.py

---
+++ super.py
@@ -0,0 +1,19 @@
+class Answer:
+    def __init__(self, answer):
+        self.answer = answer
+
+    def __str__(self):
+        return f"The Definitive Answer to what is the Meaning of the universe is... 42"
+
+
+class Supercomputer(Answer):
+    def __init__(self, answer, name, rank):
+        super().__init__(answer)
+        self.name = name
+        self.rank = rank
+
+    def __str__(self):
+        return f"{self.name} is the {self.rank} ranked computer in the galaxy!"
+    
+deepthought = Supercomputer(42 ,"Deep Thought", 1)
+print(deepthought)
